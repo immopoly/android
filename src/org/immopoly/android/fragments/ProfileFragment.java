c61224fdb56e1bf67a8933502a0d730100cc8233
sebastian <bashtian86@gmail.com>
2011-11-05 17:22:04 +0100

    Use real user name for profile

--- src/org/immopoly/android/fragments/ProfileFragment.java
+++ src/org/immopoly/android/fragments/ProfileFragment.java
@@ -1,6 +1,8 @@
 package org.immopoly.android.fragments;
 
 import org.immopoly.android.R;
+import org.immopoly.android.model.ImmopolyUser;
+import org.w3c.dom.Text;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,19 +12,22 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProfileFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.fragment_profile, container, false);
 	}
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		GridView gridView = (GridView) getView().findViewById(R.id.gridview);
 		gridView.setAdapter(new BadgeAdapter());
+		((TextView) getView().findViewById(R.id.username)).setText(ImmopolyUser.getInstance().getUserName());
 	}
-	
+
 	class BadgeAdapter extends BaseAdapter {
 
 		@Override
@@ -40,21 +45,21 @@ public class ProfileFragment extends Fragment {
 			return 0;
 		}
 
-	    public View getView(int position, View convertView, ViewGroup parent) {
-	        ImageView imageView;
-	        if (convertView == null) {  // if it's not recycled, initialize some attributes
-	            imageView = new ImageView(getActivity());
-	            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-	            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-	            imageView.setPadding(8, 8, 8, 8);
-	        } else {
-	            imageView = (ImageView) convertView;
-	        }
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ImageView imageView;
+			if (convertView == null) { // if it's not recycled, initialize some
+										// attributes
+				imageView = new ImageView(getActivity());
+				imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+				imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+				imageView.setPadding(8, 8, 8, 8);
+			} else {
+				imageView = (ImageView) convertView;
+			}
 
-	        imageView.setImageResource(R.drawable.badge_empty);
-	        return imageView;
-	    }
+			imageView.setImageResource(R.drawable.badge_empty);
+			return imageView;
+		}
 
-		
 	}
 }
