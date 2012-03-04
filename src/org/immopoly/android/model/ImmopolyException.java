12241050c5c74e1dc104dcca7a83039797ac781e
tresbjorn <tresbjorn@googlemail.com>
2012-03-04 23:10:31 +0100

    Password recovery function im Client Contant Team if recovery not possible because of missing email address fixes 39

--- src/org/immopoly/android/model/ImmopolyException.java
+++ src/org/immopoly/android/model/ImmopolyException.java
@@ -4,6 +4,7 @@ import org.immopoly.android.R;
 import org.json.JSONObject;
 
 import android.content.Context;
+import android.widget.Toast;
 
 public class ImmopolyException extends org.immopoly.common.ImmopolyException {
 
@@ -14,6 +15,9 @@ public class ImmopolyException extends org.immopoly.common.ImmopolyException {
 	public ImmopolyException(Context context, JSONObject jsonObject) {
 		super(jsonObject);
 		switch (errorCode) {
+		case USERNAME_NOT_FOUND:
+			message = context.getString(R.string.account_not_found);
+			break;
 		case 201:
 			message = context.getString(R.string.flat_already_in_portifolio);
 			break;
@@ -26,7 +30,14 @@ public class ImmopolyException extends org.immopoly.common.ImmopolyException {
 		case 441:
 			message = context.getString(R.string.expose_location_spoofing);
 			break;
+		case USER_SEND_PASSWORDMAIL_NOEMAIL:
+			message = context.getString(R.string.account_has_no_email);;
+			break;
+		case USER_SEND_PASSWORDMAIL_EMAIL_NOMATCH:
+			message = context.getString(R.string.account_has_different_email);;
+			break;
 		default:
+			Toast.makeText( context, "Exc errorCode: " + errorCode, Toast.LENGTH_LONG).show();
 		}
 	}
 }
