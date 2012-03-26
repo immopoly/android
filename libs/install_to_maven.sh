900e385587e489d77645f376bac23809d8886c7a
Stefan Hoth <sh@jnamic.com>
2012-03-26 21:19:47 +0200

    Delete jars which are now maven-dependencies again, refs #84

--- libs/install_to_maven.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#installs the libs to your local maven repo
-mvn install:install-file -Dfile=libGoogleAnalytics.jar -DgroupId=com.google.android.apps -DartifactId=analytics -Dversion=1.3.1 -Dpackaging=jar
-mvn install:install-file -Dfile=signpost-core-1.2.1.1.jar -DgroupId=oauth.signpost -DartifactId=signpost-core -Dversion=1.2.1.1 -Dpackaging=jar
-mvn install:install-file -Dfile=android-support-v4-r4-googlemaps.jar -DgroupId=android.support -DartifactId=compatibility-v4 -Dversion=r4 -Dpackaging=jar
