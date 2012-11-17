b040e9e064fab9bf3f9cbafc24524c98d2618ffa
Tobias Heine <spam@tobiasheine.eu>
2012-11-17 18:52:28 +0100

    Userprofile anzeigen
    - progress
    - orientation change

--- src/org/immopoly/android/fragments/SimpleUserFragment.java
+++ src/org/immopoly/android/fragments/SimpleUserFragment.java
@@ -8,27 +8,29 @@ import org.immopoly.android.R;
 import org.immopoly.android.constants.Const;
 import org.immopoly.android.helper.TrackingManager;
 import org.immopoly.android.model.ImmopolySimpleUser;
+import org.immopoly.android.pagination.ProgressIndicator;
 import org.immopoly.android.tasks.GetOtherUserTask;
 import org.immopoly.android.widget.BadgesView;
 
 import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 
-public class SimpleUserFragment extends DialogFragment {
+public class SimpleUserFragment extends DialogFragment implements ProgressIndicator{
 	
 	private String userName;
 	private final static String EXTRA_USERNAME="user";
 	private GoogleAnalyticsTracker mTracker;
-	private AsyncTask<String, Void, ImmopolySimpleUser> task;
 	private final static NumberFormat nFormat = NumberFormat.getCurrencyInstance(Locale.GERMANY);
+	private ProgressBar progress;
+	private ImmopolySimpleUser mUser;
 	
 	/**
 	 * Create a new instance of MyFragment that will be initialized with the
@@ -58,11 +60,14 @@ public class SimpleUserFragment extends DialogFragment {
 	}
 	
 	@Override
-	public void onViewCreated(View arg0, Bundle arg1) {
-		super.onViewCreated(arg0, arg1);
-		if(task == null){
-			task = new MyGetSimpleUserTask(getActivity()).execute(userName);
-		}
+	public void onViewCreated(View v, Bundle savedInstanceState) {
+		super.onViewCreated(v, savedInstanceState);
+		progress = (ProgressBar) getView().findViewById(R.id.progress);
+		
+		if(savedInstanceState != null && (mUser = savedInstanceState.getParcelable("user")) != null)
+			displayUserData(mUser);
+		else
+			new MyGetSimpleUserTask(getActivity()).execute(userName);
 	}
 	
 	@Override
@@ -75,6 +80,12 @@ public class SimpleUserFragment extends DialogFragment {
 	
 	class MyGetSimpleUserTask extends GetOtherUserTask{
 		
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			indicateProgress(true);
+		}
+		
 		public MyGetSimpleUserTask(Context context) {
 			super(context);
 		}
@@ -82,41 +93,60 @@ public class SimpleUserFragment extends DialogFragment {
 		@Override
 		protected void onPostExecute(ImmopolySimpleUser user) {
 			super.onPostExecute(user);
+			indicateProgress(false);
 			if(mException != null)
 				dismiss();
 			else if(user != null){
-				((BadgesView)getView().findViewById(R.id.gridview)).initWithBadges(user.getBadges(), mTracker);
+				mUser = user;
 				
-				TextView currentCostTextView = (TextView) getView().findViewById(R.id.current_cost);
-				if (currentCostTextView != null) {
-					nFormat.setMinimumIntegerDigits(1);
-					nFormat.setMaximumFractionDigits(0);
-					nFormat.setCurrency(Currency.getInstance(Locale.GERMANY));
-					
-					currentCostTextView.setText(nFormat.format(user.getLastRent()));
-				}
-				
-				String calculatedCosts = nFormat.format((int) (user.getLastRent() * 30));
-				TextView calculatedCostTextView = (TextView) getView().findViewById(R.id.calculated_cost);
-				if (calculatedCostTextView != null) {
-					calculatedCostTextView.setText(calculatedCosts);
-				}
-				
-				String lastProvision = nFormat.format((int) (user.getLastProvision()));
-				TextView lastProvisionTextView = (TextView) getView().findViewById(R.id.last_provision);
-				if (lastProvisionTextView != null) {
-					lastProvisionTextView
-					.setText(lastProvision);
-				}
-				
-				TextView balanceCostTextView = (TextView) getView().findViewById(R.id.current_balance);
-				if (balanceCostTextView != null) {
-					balanceCostTextView.setText(nFormat.format(user.getBalance()));
-				}
-				
-				TextView numFlatsView = (TextView) getView().findViewById( R.id.num_flats );
-				numFlatsView.setText( user.getNumExposes() +" / " + user.getMaxExposes() );
+				displayUserData(mUser);
 			}
 		}
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle arg0) {
+		super.onSaveInstanceState(arg0);
+		if(mUser != null)
+			arg0.putParcelable("user", mUser);
+	}
+	
+	@Override
+	public void indicateProgress(boolean isBusy) {
+		progress.setVisibility(isBusy ? View.VISIBLE : View.GONE);
+	}
+
+	private void displayUserData(ImmopolySimpleUser user) {
+		((BadgesView)getView().findViewById(R.id.gridview)).initWithBadges(user.getBadges(), mTracker);
+		
+		TextView currentCostTextView = (TextView) getView().findViewById(R.id.current_cost);
+		if (currentCostTextView != null) {
+			nFormat.setMinimumIntegerDigits(1);
+			nFormat.setMaximumFractionDigits(0);
+			nFormat.setCurrency(Currency.getInstance(Locale.GERMANY));
+			
+			currentCostTextView.setText(nFormat.format(user.getLastRent()));
+		}
+		
+		String calculatedCosts = nFormat.format((int) (user.getLastRent() * 30));
+		TextView calculatedCostTextView = (TextView) getView().findViewById(R.id.calculated_cost);
+		if (calculatedCostTextView != null) {
+			calculatedCostTextView.setText(calculatedCosts);
+		}
+		
+		String lastProvision = nFormat.format((int) (user.getLastProvision()));
+		TextView lastProvisionTextView = (TextView) getView().findViewById(R.id.last_provision);
+		if (lastProvisionTextView != null) {
+			lastProvisionTextView
+			.setText(lastProvision);
+		}
+		
+		TextView balanceCostTextView = (TextView) getView().findViewById(R.id.current_balance);
+		if (balanceCostTextView != null) {
+			balanceCostTextView.setText(nFormat.format(user.getBalance()));
+		}
+		
+		TextView numFlatsView = (TextView) getView().findViewById( R.id.num_flats );
+		numFlatsView.setText( user.getNumExposes() +" / " + user.getMaxExposes() );
 	}
 }
