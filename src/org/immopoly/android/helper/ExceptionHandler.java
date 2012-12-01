9be5245922abb7ac608546a5b644ba7e23c31f0f
Tobias Heine <spam@tobiasheine.eu>
2012-12-01 16:57:27 +0100

    https://github.com/immopoly/android/issues/87

---
+++ src/org/immopoly/android/helper/ExceptionHandler.java
@@ -0,0 +1,28 @@
+package org.immopoly.android.helper;
+
+import org.immopoly.android.app.UserDataManager;
+import org.immopoly.android.model.ImmopolyException;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ExceptionHandler {
+	
+	public static void handleException(Context ctx, ImmopolyException ex){
+		if(null == ex){
+			Toast.makeText(ctx, "Error", Toast.LENGTH_LONG).show();
+			return;
+		}
+		
+		switch (ex.getErrorCode()) {
+		case ImmopolyException.TOKEN_NOT_FOUND:
+			UserDataManager.instance.logout();
+			break;
+
+		default:
+			Toast.makeText(ctx, ex.getMessage(), Toast.LENGTH_LONG).show();
+			break;
+		}
+	}
+
+}
